d30fc652c8e83bb5b40f73a27ec330fd7657db24
tami-cp0 <findtamilore>
2025-01-26 18:57:53 +0100

    config and minor code changes

--- supabase/customSQL/bucketPolicies/avatar/deletePolicy.sql
+++ supabase/customSQL/bucketPolicies/avatar/deletePolicy.sql
@@ -1,4 +1,4 @@
 CREATE POLICY "User can delete an avatar"
 ON storage.objects
 FOR DELETE
-USING (bucket_id = 'avatars' AND owner = auth.uid());
+USING (bucket_id = 'avatars'::text AND owner_id = auth.uid()::text);
